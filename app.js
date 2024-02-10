055e78671304035a5230bd354d7206ac9cf9baf4
abid1475 <abid7991475@gmail.com>
2024-02-09 23:07:50 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+          gsap.to(".imagecontainer",{
+            ease:Expo.easeInOut,
+            width:"100%",
+            duration:2,
+            stagger:2
+          })
+gsap.to(".text h1",{
+    delay:2,
+    ease:Expo.easeInOut,
+    stagger:2,
+    top:"-100%"
+})
